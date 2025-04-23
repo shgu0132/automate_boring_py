d2941b101a1ef34431d6bd40fa2abcb882dcfd16
shgu0132 <g.shivendra@gmail.com>
2025-04-23 13:17:25 +0530

    completed chapter 1 exercises

---
+++ ch1/hello_world.py
@@ -0,0 +1,15 @@
+# This program says hello and asks for the user's name.
+
+print("Hello, world!")
+print("What is your name?") # Ask for the user's name
+name = input() # Get the user's name
+print("It's good to meet you, " + name + ".") # Greet the user
+print('The length of your name is: ' + str(len(name)) + '.') # Tell the user the length of their name
+print() # Print a blank line
+print("What is your age?") # Ask for the user's age
+age = input() # Get the user's age
+print("You will be " + str(int(age) + 1) + " in a year.") # Tell the user their age next year
+print()
+
+# This is the end of the program
+
